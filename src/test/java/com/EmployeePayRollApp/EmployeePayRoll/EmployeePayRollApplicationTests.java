a3704768f0c94f81be1121bb48dacb31fd1cd659
puneet0410 <puneetp2200@gmail.com>
2025-03-08 16:56:30 +0530

    Initial Commit

---
+++ src/test/java/com/EmployeePayRollApp/EmployeePayRoll/EmployeePayRollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EmployeePayRollApp.EmployeePayRoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayRollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
